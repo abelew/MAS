77620c7a334d0c4b89cd0e7ec41644630684c431
Ashton Trey Belew <abelew@gmail.com>
2021-06-27 13:22:18 -0400

    made a couple of notes to myself

--- amd_database_scripts/easy_upload.py
+++ amd_database_scripts/easy_upload.py
@@ -47,7 +47,46 @@ def run_glimmer(fasta_path, phage_name, output_destination, minimum_gene_len=90)
 
     long_orf_file.close()
     extract_file.close()
-
+    # Unless I am mistaken, it may be worth while to add to this the second round of glimmer;
+    # I think this, as written, is intended to provide the training hmm for a more refined
+    # search by glimmer, as per Arthur Delcher's release notes:
+    # https://ccb.jhu.edu/software/glimmer/glim302notes.pdf
+    #
+    # Thus I think one would add a variant of the end of 'g3-iterated.csh', e.g.:
+    # tail +2 run3.run1.predict > run3.coords
+    # upstream-coords.awk 25 0 run3.coords | extract genom.seq - > run3.upstream
+    # elph run3.upstream LEN=6 | get-motif-counts.awk > run3.motif
+    # set startuse = $(start-codon-distrib -3 genom.seq run3.coords)
+    # glimmer3 -o50 -g110 -t30 -b run3.motif -P ${startuse} genom.seq run3.icm run3
+    #
+    # My own script wraps the two iterations in a set of invocations of all the pieces
+    # which when combined, look something like this:
+    #
+    # mkdir -p outputs/glimmer
+    # long-orfs -n -t 1.15 outputs/shovill_r1_trimmed-corrected/contigs.fa outputs/glimmer/first_run_longorfs.txt
+    # extract outputs/shovill_r1_trimmed-corrected/contigs.fa outputs/glimmer/first_run_longorfs.txt \
+    #    1>outputs/glimmer/first_run_training.txt 2>outputs/glimmer/first_run_training.err
+    # build-icm -r outputs/glimmer/first_run.icm < outputs/glimmer/first_run_training.txt \
+    #    2>outputs/glimmer/first_run_icm.out 1>&2
+    # glimmer3 -o50 -g110 -t30 \
+    #    outputs/shovill_r1_trimmed-corrected/contigs.fa \
+    #    outputs/glimmer/first_run.icm \
+    #    outputs/glimmer/first_run.out
+    # ##  Use this first run output to go again
+    # extract -t outputs/shovill_r1_trimmed-corrected/contigs.fa \
+    #    outputs/glimmer/first_run.out.predict train.coords \
+    #    1>outputs/glimmer/second_run_training.txt \
+    #    2>outputs/glimmer/second_run_training.err
+    # build-icm -r outputs/glimmer/second_run.icm < outputs/glimmer/second_run_training.txt
+    # upstream-coords.awk 25 0 outputs/glimmer/first_run.out.predict | extract outputs/shovill_r1_trimmed-corrected/contigs.fa - > \
+    #    outputs/glimmer/second_run_upstream.txt
+    # elph outputs/glimmer/second_run_upstream.txt LEN=6 2>outputs/glimmer/elph.err | \
+    #    get-motif-counts.awk 2>outputs/glimmer/second_run_motif.txt 1>&2
+    # startuse=$(start-codon-distrib -3 outputs/shovill_r1_trimmed-corrected/contigs.fa outputs/glimmer/first_run.out.predict)
+    # glimmer3 -o50 -g110 -t30 -b outputs/glimmer/second_run_motif.txt -P ${startuse} \
+    #    outputs/shovill_r1_trimmed-corrected/contigs.fa outputs/glimmer/second_run.icm \
+    #    outputs/glimmer/second_run.out \
+    #    2>outputs/glimmer/second_run.err 1>&2
     return os.path.join(output_destination, '%s.glimmer.predict' % phage_name)
 
 
@@ -55,8 +94,15 @@ def run_trnascan_se(fasta_path, phage_name, output_destination):
     '''
     Run tRNAscan-SE and return output file
     '''
-    print('*** RUNNING GLIMMER ***')
+    print('*** RUNNING tRNAscan ***')
 
     out_file_path = os.path.join(output_destination, '%s.trnascan_se.results.txt' % phage_name)
     subprocess.run(['tRNAscan-SE', '-B', '-D', '-o', out_file_path, fasta_path], check=True)
     return out_file_path
+
+
+# I think I want to add aragorn here in the hopes that it will be a
+# bit better at picking up mtRNAs, which I imagine are useful for
+# phages once the bacteria 'realize' there are shenanigans afoot and
+# shut down translation.  In my brief reading of trnascan-se, I did
+# not get the sense that it looks for them as effectively.
